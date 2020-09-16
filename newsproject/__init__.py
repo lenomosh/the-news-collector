0f9d064ab06d288899e551f121650c05c3b2d1a6
Lennox <omondilenox@outlook.com>
2020-09-16 12:55:03 +0300

    indented newsporject.__init__.py

--- newsproject/__init__.py
+++ newsproject/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from instance.config import DevConfig
+
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
@@ -10,7 +11,6 @@ Migrate(app, db)
 from newsproject import views
 from newsproject.news_article.views import news_article_blueprint
 from newsproject.news_source.views import news_source_blueprint
+
 app.register_blueprint(news_article_blueprint, url_prefix='/articles')
 app.register_blueprint(news_source_blueprint, url_prefix='/news_source')
-
-# @news_source_blueprint.route('/category>')
