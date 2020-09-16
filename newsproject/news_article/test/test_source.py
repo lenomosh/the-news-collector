f3c03e8a9f2e37c1a5f00efe46da084d26ab7e5c
Lennox <omondilenox@outlook.com>
2020-09-16 19:50:53 +0300

    formatted test source file pep8e

--- newsproject/news_article/test/test_source.py
+++ newsproject/news_article/test/test_source.py
@@ -9,14 +9,14 @@ class TestArticle(unittest.TestCase):
         SetUp method that will run before every Test
         '''
         # (self, category, country, description, id, language, name, url):
-        return Source("general","us","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","abc-news","en","ABC News","https://abcnews.go.com")
+        return Source("general", "us",
+                      "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos "
+                      "at ABCNews.com.",
+                      "abc-news", "en", "ABC News", "https://abcnews.go.com")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.setUp(), Source))
 
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
